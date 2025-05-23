12808d96272e9849122d1009aef6a0be67d38765
IbrahimDilshad <ibrahimzdilshad@gmail.com>
2025-05-24 03:15:57 +0500

    update

--- components/ui/productheader.tsx
+++ components/ui/productheader.tsx
@@ -36,7 +36,9 @@ export function TypewriterEffectSmoothDemo() {
       <TypewriterEffectSmooth words={words} />
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
         <a
-          href="https://wa.me/923289451617"
+          href="https://wa.me/923289451617?text=I'm%20interested%20in%20selling%20my%20product%20on%20SkillSphere"
+          target="_blank"
+          rel="noopener noreferrer"
           className="w-40 h-10 rounded-xl bg-black border border-white text-white text-sm flex items-center justify-center"
         >
           Sell Yours
